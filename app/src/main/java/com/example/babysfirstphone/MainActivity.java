5fc827241308120951df2b57884899e92f93ac3f
Adan <adanmisael@gmail.com>
2021-02-21 01:04:40 -0800

    Sample GUI

--- app/src/main/java/com/example/babysfirstphone/MainActivity.java
+++ app/src/main/java/com/example/babysfirstphone/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.babysfirstphone;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Adan
+
+        imageButton = (ImageButton) findViewById(R.id.image_button_android);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "It Works", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
