d30647f44b162f26212734163caed0fe20df1810
jellybean911 <56736809+jellybean911@users.noreply.github.com>
2021-02-25 23:05:38 -0800

    Add emergency call function under emergency tab.

--- app/src/main/java/com/example/babysfirstphone/EmergencyFragment.java
+++ app/src/main/java/com/example/babysfirstphone/EmergencyFragment.java
@@ -2,24 +2,54 @@ package com.example.babysfirstphone;
 
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+
+import android.net.Uri;
+import android.Manifest;
+import android.widget.ImageButton;
+import android.Manifest;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EmergencyFragment extends Fragment {
 
     FloatingActionButton fab;
+    ImageButton dadBtn;
+    ImageButton momBtn;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_emergency, container, false);
+
+        dadBtn = (ImageButton) view.findViewById(R.id.dadBtn);
+        dadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callPhoneNumber("1-989-390-8150");
+            }
+        });
+
+        momBtn = (ImageButton) view.findViewById(R.id.momBtn);
+        momBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callPhoneNumber("1-906-370-1986");
+            }
+        });
+
 
         fab = (FloatingActionButton) view.findViewById(R.id.home_button);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +60,38 @@ public class EmergencyFragment extends Fragment {
         });
 
         return view;
+
+
     }
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        if (requestCode == 101) {
+//            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                callPhoneNumber();
+//            }
+//        }
+//    }
+
+    public void callPhoneNumber(String phoneNum) {
+        try {
+            if (Build.VERSION.SDK_INT > 22) {
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) !=PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{
+                            Manifest.permission.CALL_PHONE}, 101);
+                    return;
+                }
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + phoneNum));
+                startActivity(callIntent);
+            } else {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + phoneNum));
+                startActivity(callIntent);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
 
 }
