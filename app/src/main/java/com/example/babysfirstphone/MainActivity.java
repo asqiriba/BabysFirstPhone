b8cfc5439a531dd1dc390e20c9b601869f08c467
Hassan Asqiriba <federiz@msn.com>
2021-03-15 15:16:12 -0700

    Creation of ContactsAdapter.java, which is the handler of Contacts created by the user. This is NOT the final version.

--- app/src/main/java/com/example/babysfirstphone/MainActivity.java
+++ app/src/main/java/com/example/babysfirstphone/MainActivity.java
@@ -9,22 +9,74 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.view.View;
+import android.widget.ListView;
+import android.view.ContextMenu;
+import android.widget.Toast;
 
+import com.example.babysfirstphone.contacts.ContactDetails;
+import com.example.babysfirstphone.contacts.ContactsAdapter;
 import com.example.babysfirstphone.controllers.Caller;
+import com.example.babysfirstphone.controllers.Contacts;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     // We make a Caller object, from Controllers/Caller.java
     Caller caller = new Caller();
 
+    // We used ArrayList because its size can be increased or reduced dynamically.
+    ArrayList<Contacts> arrayListContact;
+    ContactsAdapter contactAdapter;
+    Contacts contacts;
+
+    Button contactAddButton;
+    ListView listContacts;
+
+    final int CONTACT_VIEW = 1, CONTACT_DELETE = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        arrayListContact = new ArrayList<Contacts>();
+        listContacts = (ListView) findViewById(R.id.listView);
+        contactAddButton = (Button) findViewById(R.id.contactAddButton);
+
+        /*
+            Click Listener on Contact Button.
+            User click on Contact Button, it takes user to the ContactDataActivity, where we take
+            input from the user.
+         */
+        contactAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContactDataActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        // Here we called the constructor of ContactsAdpater class, in  which we passed the context and ArrayList.
+        contactAdapter = new ContactsAdapter(MainActivity.this, arrayListContact);
+
+        listContacts.setAdapter(contactAdapter);
+
+        listContacts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                registerForContextMenu(listContacts);
+            }
+        });
 
         // Call interface
         ImageButton im_contact = (ImageButton) findViewById(R.id.image_button_android);
@@ -79,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             startActivity(caller.makeCall());
+
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -88,5 +141,62 @@ public class MainActivity extends AppCompatActivity {
     public void testContactActivity(View view) {
         Intent intent = new Intent(getBaseContext(), ContactDataActivity.class);
         startActivity(intent);
+    }
+
+    /*
+        Menu render when long-press.
+     */
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        if (v.getId() == R.id.listView) {
+            menu.add(0, CONTACT_VIEW, 1, "View");
+            menu.add(0, CONTACT_DELETE, 2, "Delete");
+        }
+    }
+
+    /*
+        Here i implemented the Menu option, when user do a long Press on contacts.
+        In here we write the code for each Menu.
+     */
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case CONTACT_VIEW:
+                Intent intent = new Intent(MainActivity.this, ContactDetails.class);
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                int index1 = info.position;
+
+                intent.putExtra("details", arrayListContact.get(index1));
+
+                startActivity(intent);
+                break;
+
+            case CONTACT_DELETE:
+                Toast.makeText(MainActivity.this, "Delete",Toast.LENGTH_SHORT).show();
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                int index = info.position;
+
+                Log.e("index", index + " ");
+                arrayListContact.remove(index);
+                contactAdapter.notifyDataSetChanged();
+                break;
+        }
+        return true;
+    }
+
+    /*
+        We receive the data coming from ContactDataActivity.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == 2) {
+            contacts = (Contacts) data.getSerializableExtra("data");
+            arrayListContact.add(contacts);
+            contactAdapter.notifyDataSetChanged();
+        }
     }
 }
