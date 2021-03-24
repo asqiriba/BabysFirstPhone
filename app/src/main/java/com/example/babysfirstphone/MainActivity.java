372456ac0420b63d948f1acacb4fb74bf1b8bdde
Hassan Asqiriba <federiz@msn.com>
2021-03-23 21:53:58 -0700

    Clean Main Screen for future merges.

--- app/src/main/java/com/example/babysfirstphone/MainActivity.java
+++ app/src/main/java/com/example/babysfirstphone/MainActivity.java
@@ -48,40 +48,6 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-//        arrayListContact = new ArrayList<Contacts>();
-//        listContacts = (ListView) findViewById(R.id.listView);
-//        contactAddButton = (Button) findViewById(R.id.contactAddButton);
-
-        /*
-            Click Listener on Contact Button.
-            User click on Contact Button, it takes user to the ContactDataActivity, where we take
-            input from the user.
-         */
-//        contactAddButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, ContactDataActivity.class);
-//                startActivityForResult(intent, 1);
-//            }
-//        });
-//
-//        // Here we called the constructor of ContactsAdpater class, in  which we passed the context and ArrayList.
-//        contactAdapter = new ContactsAdapter(MainActivity.this, arrayListContact);
-//
-//        listContacts.setAdapter(contactAdapter);
-//
-//        listContacts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//
-//                registerForContextMenu(listContacts);
-//            }
-//        });
-
-//        // Call interface
-//        ImageButton im_contact = (ImageButton) findViewById(R.id.image_button_android);
-//        im_contact.setOnClickListener(view -> callPhoneNumber());
-
         // Bottom Menu
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -137,70 +103,4 @@ public class MainActivity extends AppCompatActivity {
             ex.printStackTrace();
         }
     }
-
-//    /** Called when the user taps the helper button */
-//    public void testContactActivity(View view) {
-//        Intent intent = new Intent(getBaseContext(), ContactDataActivity.class);
-//        startActivity(intent);
-//    }
-
-//    /*
-//        Menu render when long-press.
-//     */
-//    @Override
-//    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-//        super.onCreateContextMenu(menu, v, menuInfo);
-//
-//        if (v.getId() == R.id.listView) {
-//            menu.add(0, CONTACT_VIEW, 1, "Call");
-//            menu.add(0, CONTACT_DELETE, 2, "Delete");
-//        }
-//    }
-//
-//    /*
-//        Here i implemented the Menu option, when user do a long Press on contacts.
-//        In here we write the code for each Menu.
-//     */
-//    @Override
-//    public boolean onContextItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case CONTACT_VIEW:
-////                Intent intent = new Intent(MainActivity.this, ContactDetails.class);
-////                AdapterView.AdapterContextMenuInfo infoView = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-////                int index1 = infoView.position;
-////
-////                intent.putExtra("details", arrayListContact.get(index1));
-////
-////                startActivity(intent);
-//        // Call interface
-/////TODO: Make phone call available to each contact.
-//                callPhoneNumber();
-//                break;
-//
-//            case CONTACT_DELETE:
-//                Toast.makeText(MainActivity.this, "Delete", Toast.LENGTH_SHORT).show();
-//                AdapterView.AdapterContextMenuInfo infoDelete = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-//                int index = infoDelete.position;
-//
-//                Log.e("index", index + " ");
-//                arrayListContact.remove(index);
-//                contactAdapter.notifyDataSetChanged();
-//                break;
-//        }
-//        return true;
-//    }
-//
-//    /*
-//        We receive the data coming from ContactDataActivity.
-//     */
-//    @Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (resultCode == 2) {
-//            contacts = (Contacts) data.getSerializableExtra("data");
-//            arrayListContact.add(contacts);
-//            contactAdapter.notifyDataSetChanged();
-//        }
-//    }
 }
