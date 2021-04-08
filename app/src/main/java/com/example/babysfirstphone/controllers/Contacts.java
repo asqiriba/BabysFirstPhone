9b8a1285bcca2c27252b327d4a916f72869eb30c
Hassan Asqiriba <federiz@msn.com>
2021-04-07 20:45:45 -0700

    Set default Contacts Image for Contact Create functionality. This commit also updates the README.md.

--- app/src/main/java/com/example/babysfirstphone/controllers/Contacts.java
+++ app/src/main/java/com/example/babysfirstphone/controllers/Contacts.java
@@ -37,7 +37,12 @@ public class Contacts implements Serializable {
     }
 
     public int getImage() {
-        return contactImage;
+        if(contactImage != 0) {
+            return contactImage;
+        }
+        else {
+            return 2131231004;
+        }
     }
 
     public void setImageId(int imageId) {
