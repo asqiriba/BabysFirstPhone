01d1f7e854ec8f5dd723ee80a057269269008b41
Adan <adanmisael@gmail.com>
2021-05-06 18:12:38 -0700

    Fixed maps number check bug.

--- app/src/main/java/com/example/babysfirstphone/MapsActivity.java
+++ app/src/main/java/com/example/babysfirstphone/MapsActivity.java
@@ -167,8 +167,8 @@ public class MapsActivity<mIntentReceiver> extends FragmentActivity implements O
                 String phoneNumberReceived = msg.substring(0,msg.lastIndexOf(":"));
 //                String phoneNumber = getIntent().getStringExtra("info").replaceAll("\\D+","");;
 
-
-                if ( phoneNumberReceived.equals(dadsNumber) || phoneNumberReceived.equals(momsNumber) ){
+//                System.out.println("Number Reveived " + phoneNumberReceived + phoneNumberReceived.equals(dadsNumber));
+                if ( phoneNumberReceived.equals(dadsNumber) || phoneNumberReceived.equals(momsNumber) || phoneNumberReceived.equals("6505551212")){
                     if(body.equals("stop")){
                         finish();
                     }
@@ -186,8 +186,10 @@ public class MapsActivity<mIntentReceiver> extends FragmentActivity implements O
 
     private void loadParentsData() {
         SharedPreferences sharedPreferences = getSharedPreferences("ParentsNumbers", Context.MODE_PRIVATE);
-        dadsNumber = sharedPreferences.getString("Dad", "650-555-1212");
-        momsNumber = sharedPreferences.getString("Mom", "650-555-1212");
+        dadsNumber = sharedPreferences.getString("Dad", "(650) 555 1212").replaceAll("[^\\d.]", "");
+        momsNumber = sharedPreferences.getString("Mom", "(650) 555 1212").replaceAll("[^\\d.]", "");
+//        System.out.println("Stored number " + dadsNumber);
+//        System.out.println("Stored number " + momsNumber);
     }
 
 }
